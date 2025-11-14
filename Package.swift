ab954748c78f44896fa22f3e8e57dfe35bba8c63
Filip Stojanovski <filip.stojanovski2@infinum.hr>
2025-11-14 23:28:29 +0100

    Add zip file. Add Package.swift.

---
+++ Package.swift
@@ -0,0 +1,26 @@
+//
+// Created by Filip Stojanovski on 14.11.25.
+//
+
+import PackageKit
+import Foundation
+
+let package = Package(
+    name: "SharedComposeApp",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "SharedComposeApp",
+            targets: ["SharedComposeApp"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SharedComposeApp",
+            url: "https://yourserver.com/SharedComposeApp.xcframework.zip",
+            checksum: "475b424c6f8f6e92110acc3507513592b30336bea51759a86ccaf0aa7a76b433"
+        )
+    ]
+)
