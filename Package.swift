c9c7fd12b307fc830c5ca1ac69b112376c7cb0f1
Filip Stojanovski <filip.stojanovski2@infinum.hr>
2025-11-14 23:45:13 +0100

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -1,24 +1,23 @@
 // swift-tools-version: 5.9
-
-import PackageKit
-import Foundation
+import PackageDescription
 
 let package = Package(
-    name: "SharedComposeApp",
+    name: "PoVPOC", // Matches the repository name
     platforms: [
-        .iOS(.v14)
+        .iOS(.v16), // Adjust if you want lower versions
+        .macOS(.v14)
     ],
     products: [
         .library(
-            name: "SharedComposeApp",
+            name: "SharedComposeApp", // Name clients will import
             targets: ["SharedComposeApp"]
         )
     ],
     targets: [
         .binaryTarget(
             name: "SharedComposeApp",
-            url: "https://github.com/Filip2Stojanovski/PoVPOC/blob/main/SharedComposeApp.xcframework.zip",
-            checksum: "475b424c6f8f6e92110acc3507513592b30336bea51759a86ccaf0aa7a76b433"
+            url: "https://github.com/Filip2Stojanovski/PoVPOC/raw/main/SharedComposeApp.xcframework.zip",
+            checksum: "<PUT_YOUR_CHECKSUM_HERE>"
         )
     ]
 )
